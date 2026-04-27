0b562b292b65b6f75dfed3e425bb8bb6dc9411f8
Massimiliano <maxtera87@gmail.com>
2026-04-27 10:59:02 +0100

    edit axios to railway instead localhost

--- front_end/client/src/api/axios.js
+++ front_end/client/src/api/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+
 const api = axios.create({
   baseURL: 'http://localhost:3001/api',
 });
